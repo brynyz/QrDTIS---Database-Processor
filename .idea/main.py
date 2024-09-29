15157a8695f6a16aecbfe285b52e4e019c2da88e
Brylle <bryllenyelm@gmail.com>
2024-09-29 23:25:24 +0800

    initial commit

---
+++ .idea/main.py
@@ -0,0 +1,7 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
+from PySide6.uic import loadUi
+
+class login()
